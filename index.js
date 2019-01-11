57d42f914a761faead7dba3f35866b9525e95e4c
JS <h1@seattle1.com>
2019-01-10 17:45:06 -0800

    Nothing

--- index.js
+++ index.js
@@ -1,76 +1,110 @@
 "use strict";
-var webSocketsServerPort = +process.env.PORT || 8081;
-var webSocketServer = require('websocket').server;
-var http = require('http');
-var clients = [];
+var webSocketsServerPort = +process.env.PORT || 8081;//The port that this entire server will run on...
+var webSocketServer = require('websocket').server;//Create a webSocket server
+var http = require('http');//Import the http module
+var clients = [];//Keep track of all signed in clients
+var turn = 0;//Keep track of whose turn it is
+var boards = [
+    [
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,6,6,6,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,7,0],
+        [0,5,0,0,0,0,0,0,7,0],
+        [0,5,0,0,0,0,0,0,7,0],
+        [0,5,0,0,4,4,0,0,7,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,8,8,8,8,8,0,0]
+    ],
+    [
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,5,0,0,0,0,0,0,0],
+        [0,0,5,0,0,4,4,0,0,0],
+        [0,0,5,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,7,0,0,0,0],
+        [0,0,0,0,0,7,0,0,0,0],
+        [0,0,0,0,0,7,0,0,6,0],
+        [8,8,8,8,8,7,0,0,6,0],
+        [0,0,0,0,0,0,0,0,6,0]
+    ]
+];//Keep track of both user's boards (Will eventually be recieved from the players...)
 
-function htmlEntities(str) {
+function htmlEntities(str) {//Cleans a string
     return String(str).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;");
 }
 function prepareMessage(obj){
     /*Prepare the message for use.
-    return 1 and the message if the message is a move,
+    return 1 and the move x and y if the message is a move,
     return 2 and the cleaned message if the message is a text,
-    return 0 and an empty string if the message is a move, but not a valid one.
+    return 0 if the message is a move, but not a valid one.
     */
-    if(movex && movey && typeof movex === "number" && typeof movey === "number" && obj.movex < 11 && obj.movey < 11){
+    if(obj.movex && obj.movey && typeof obj.movex === "number" && typeof obj.movey === "number" && obj.movex < 11 && obj.movey < 11){//Make sure the message IS a move. NO EXCEPTIONS. Since this string isn't cleaned it MUST be clean to make it to the moves. It must also be between 1-10
         return {
             type: 1,
             movex: obj.movex,
             movey: obj.movey
         };
-    }else if(obj.value && typeof obj.value === "string"){
+    }else if(obj.value && typeof obj.value === "string"){//Make sure the input is a string before it can be cleaned.
         return {
             type: 2,
-            value: htmlEntities(obj.value)
+            value: htmlEntities(obj.value)//Clean the string
         };
-    }else{
+    }else{//If it's not a move or a text, it will be ignored
         return(0);
     }
 }
-var server = http.createServer(function(request,response){});
-server.listen(webSocketsServerPort, function() {
-    console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
+var server = http.createServer(function(request,response){});//Create an HTTP server.
+server.listen(webSocketsServerPort, function() {//Listen on port 8081
+    console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);//Let me know so that I know my code is working...
 });
-var wsServer = new webSocketServer({
+var wsServer = new webSocketServer({//Create a webSocket server on the HTTP one above.
     httpServer: server
 });
 
-wsServer.on("request",function(request){
-    if(clients.length > 1){
+wsServer.on("request",function(request){//When a user joins...
+    if(clients.length > 1){//Make sure there aren't already 2 people playing
         return;
     }
-    var connection = request.accept(null,request.origin);
-    var index = clients.push(connection) - 1;
-    var userName = "Player " + (index + 1);
-    connection.on('message',function(message){
-        if(message.type === 'utf8'){
-            var mes = prepareMessage(message);
-            if(mes.type === 0){
+    var connection = request.accept(null,request.origin);//Accept the connection
+    var index = clients.push(connection) - 1;//Add the client's index so it can be accessed later. (And tracked)
+    var userName = "Player " + (index + 1);//Give the user a name
+    connection.on('message',function(message){//When a user sends a message...
+        if(message.type === 'utf8'){//Make sure it's text
+            var mes = prepareMessage(message);//Prepare the message
+            if(mes === 0){//If the message is not a move or a text, quit.
                 return;
-            }
-            if(mes.type === 1){
+            }else if(mes.type === 1){//If the message is a move...
+                if(turn !== index){
+                    return;
+                }
+                var x = mes.movex;
+                var y = mes.movey;
+                var value = boards[!turn][y][x];
+                if(value < 4 && value > 8){
+                    return;
+                }
                 var obj = {
-                    movex: mes.movex,
-                    movey: mes.movey,
-                    author: userName
+                    result: value,
+                    author: index
                 };
-            }else if(mes.type === 2){
+            }else if(mes.type === 2){//If the message is a text...
                 var obj = {
                     text: mes.value,
                     author: userName
                 };
             }
-            var json = JSON.stringify({
+            var json = JSON.stringify({//Stringify the obj...
                 type: "text",
                 data: obj
             });
-            for(var i=0; i < clients.length; i++){
+            for(var i=0; i < clients.length; i++){//Send it to all the connected clients...
                 clients[i].sendUTF(json);
             }
         }
     });
-    connection.on('close', function(connection) {
-        clients.splice(index, 1);
+    connection.on('close', function(connection){//When a user leaves...
+        clients.splice(index, 1);//Delete them from the client list
     });
 });
